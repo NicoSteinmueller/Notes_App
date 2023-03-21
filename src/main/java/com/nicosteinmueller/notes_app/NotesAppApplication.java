b785f1cce2abf202cff1f5c72ce785d938c9f215
Nico Steinmüller <nico.steinmueller@gmx.net>
2023-03-21 19:12:32 +0100

    init

---
+++ src/main/java/com/nicosteinmueller/notes_app/NotesAppApplication.java
@@ -0,0 +1,13 @@
+package com.nicosteinmueller.notes_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesAppApplication.class, args);
+	}
+
+}
