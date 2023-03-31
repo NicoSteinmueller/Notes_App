de8a17430593c80fcbdca3dbc99acbd2896f650d
Nico Steinmüller <nico.steinmueller@gmx.net>
2023-03-31 02:06:31 +0200

    add All args Constructor

--- src/main/java/com/nicosteinmueller/notes_app/Models/api/NoteShortApi.java
+++ src/main/java/com/nicosteinmueller/notes_app/Models/api/NoteShortApi.java
@@ -1,11 +1,13 @@
 package com.nicosteinmueller.notes_app.Models.api;
 
 import com.nicosteinmueller.notes_app.Models.Note;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
 public class NoteShortApi {
     private String id;
     private String title;
