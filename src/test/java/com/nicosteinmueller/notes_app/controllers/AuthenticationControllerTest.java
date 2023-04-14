ad4afb756d50c2e8df229cf6ec080614181cd40e
Nico Steinmüller <nico.steinmueller@gmx.net>
2023-04-14 16:47:34 +0200

    add AuthenticationController Tests

--- src/test/java/com/nicosteinmueller/notes_app/controllers/AuthenticationControllerTest.java
+++ src/test/java/com/nicosteinmueller/notes_app/controllers/AuthenticationControllerTest.java
@@ -1,19 +1,25 @@
 package com.nicosteinmueller.notes_app.controllers;
 
 import com.nicosteinmueller.notes_app.Controllers.AuthenticationController;
-import com.nicosteinmueller.notes_app.Models.Language;
-import com.nicosteinmueller.notes_app.Models.Role;
-import com.nicosteinmueller.notes_app.Models.Settings;
 import com.nicosteinmueller.notes_app.Models.User;
+import com.nicosteinmueller.notes_app.Models.api.AuthenticationRequest;
+import com.nicosteinmueller.notes_app.Models.api.AuthenticationResponse;
+import com.nicosteinmueller.notes_app.Models.api.RegisterRequest;
 import com.nicosteinmueller.notes_app.Repositorys.UserRepository;
 import com.nicosteinmueller.notes_app.Security.auth.AuthenticationService;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
 
-import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -25,12 +31,65 @@ public class AuthenticationControllerTest {
     @InjectMocks
     AuthenticationController authenticationController;
 
-    @BeforeEach
-    void setUp(){
-        Settings settings = new Settings(true, Language.ENGLISH);
-        User user = new User("123", "last", "first", "first@last.net", "passwordHash",
-                LocalDateTime.of(2023,03,20,12,00, 00), settings, Role.USER);
 
+    @Test
+    void registerSuccess(){
+        Mockito.when(userRepository.findUserByEmail(Mockito.anyString())).thenReturn(Optional.empty());
+        var authResp = AuthenticationResponse.builder()
+                .token("token")
+                .build();
+        Mockito.when(authenticationService.register(Mockito.any())).thenReturn(authResp);
+        var request = RegisterRequest.builder()
+                .firstName("first")
+                .lastName("last")
+                .email("first@last")
+                .password("passwordHash")
+                .build();
 
+        var response = authenticationController.register(request);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(authResp, response.getBody());
     }
+
+    @Test
+    void registerFailBecauseUserAlreadyExists(){
+        Mockito.when(userRepository.findUserByEmail(Mockito.anyString())).thenReturn(Optional.of(new User()));
+        var request = RegisterRequest.builder()
+                .firstName("first")
+                .lastName("last")
+                .email("first@last")
+                .password("passwordHash")
+                .build();
+
+        var response = authenticationController.register(request);
+        assertEquals(HttpStatus.CONFLICT, response.getStatusCode());
+    }
+
+    @Test
+    void authenticateSuccess(){
+        var authResp = AuthenticationResponse.builder()
+                .token("token")
+                .build();
+        Mockito.when(authenticationService.authenticate(Mockito.any())).thenReturn(authResp);
+        var request = AuthenticationRequest.builder()
+                .email("new@mail")
+                .password("passHash")
+                .build();
+
+        var response = authenticationController.authenticate(request);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(authResp, response.getBody());
+    }
+
+    @Test
+    void authenticationFailBecauseInvalidCredentials(){
+        Mockito.when(authenticationService.authenticate(Mockito.any(AuthenticationRequest.class))).thenThrow(BadCredentialsException.class);
+        var request = AuthenticationRequest.builder()
+                .email("new@mail")
+                .password("passHash")
+                .build();
+        assertThrows(BadCredentialsException.class, () -> authenticationController.authenticate(request));
+    }
+
 }
